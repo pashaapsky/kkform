610d8d0789f2a1462513607262bef3b8d68f8e2a
pashaapsky <ap.sky@yandex.ru>
2021-02-24 22:11:01 +0300

    finishing

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     const form = document.querySelector('form');
 
     async function login(values) {
         const realm = "Apsky-demo";
         const clientId = "react-app";
         // const loginUri = "http://localhost:8080/auth/realms/" + realm.toLocaleLowerCase() + "/protocol/openid-connect/token";
-        const loginUri = "http://localhost:8080/auth/realms/apsky-demo/protocol/openid-connect/auth";
+        const loginUri = "http://localhost:8080/auth/realms/apsky-demo/protocol/openid-connect/token";
 
         console.log('loginUri: ', loginUri);
 
-        const data = {
-            "client_id": "react-app",
-            "username": values.name,
-            "password": values.password,
-            "grant_type": "password",
-            "response_type" : "code"
-        };
+        // const data = {
+        //     client_id: "react-app",
+        //     grant_type: "password",
+        //     username: values.name,
+        //     password: values.password
+        // };
+
+        // let data = new FormData(form);
+        // data.append("client_id", "react-app");
+        // data.append("grant_type", "password");
+
+        let data = new URLSearchParams({
+            client_id: "react-app",
+            grant_type: "password",
+            username: values.name,
+            password: values.password
+        });
 
         console.log('data:', data);
 
-        const result = await fetch(loginUri,{
-            method: "POST",
-            mode: 'no-cors',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                // 'Access-Control-Allow-Origin': '*',
-                // 'Access-Control-Allow-Headers': '*'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => console.log(res));
+        try {
+            const response = await fetch(loginUri, {
+                method: "POST",
+                // mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    // 'Content-Type': 'application/json;charset=utf-8',
+                    //     // 'Access-Control-Allow-Origin': '*',
+                    //     // 'Access-Control-Allow-Headers': '*'
+                },
+                body: data
+            });
 
-        console.log('result : ', result)
+            let result = await response.json();
+            console.log('result:', result);
+        } catch (e) {
+            console.log(e.message);
+            console.log(e);
+        }
+
     }
 
     form.addEventListener('submit', async (e) => {
@@ -47,5 +64,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         const res = await login(values);
 
+        console.log('res:', res);
     })
 });
